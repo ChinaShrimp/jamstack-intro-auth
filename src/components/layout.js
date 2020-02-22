7e8402e0d6e8d42d18fd8bfee2d937ae44efce5f
Lyon Liang <lyon.liang12@gmail.com>
2020-02-22 16:06:55 +0800

    feat: add login modal from react-netlify-identity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,16 @@
 import React from "react";
+import {IdentityContextProvider } from "react-netlify-identity-widget";
 import { Link } from "gatsby";
 
 import styles from "./layout.css";
 
 const Layout = ({ children }) => (
-  <>
+  <IdentityContextProvider url="https://jamstack-intro-auth-lyon.netlify.com">
     <header>
       <Link to="/">JAMStack App</Link>
     </header>
     <main>{children}</main>
-  </>
+  </IdentityContextProvider>
 );
 
 export default Layout;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,8 @@
-import React, {useEffect, useReducer} from "react";
+import React, {useEffect, useState, useReducer} from "react";
 import { Router } from "@reach/router";
 import { navigate } from "gatsby";
+import { IdentityModal } from "react-netlify-identity-widget";
+import "react-netlify-identity-widget/styles.css";
 
 import Layout from "../components/layout";
 import Profile from "../components/profile";
@@ -9,20 +11,26 @@ import RouteSecret from "../components/route-secret";
 import RouteLogin from "../components/route-login";
 
 const INITIAL_STATE = {
-  loginStatus: false
+  loginStatus: false,
+  showLoginDialog: false,
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "updateLoginStatus":
-      return {...state, loginStatus: action.loginStatus};
+      return { ...state, loginStatus: action.loginStatus };
+    case "updateShowLoginDialog":
+      return {...state, showLoginDialog: action.showLoginDialog};
     default:
       return INITIAL_STATE;
   }
 };
 
-const Dashbaord = () => {
+const Dashboard = () => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const [loginModalVisibility, setLoginModalVisibility] = useState(true);
+
+  const showModal = () => setLoginModalVisibility(true);
 
   useEffect(() => {
     if (!state.loginStatus) {
@@ -30,16 +38,25 @@ const Dashbaord = () => {
     }
   }, []);
 
+  const toggleLoginDialog = () => dispatch({
+    type: "updateShowLoginDialog",
+    showLoginDialog: !state.showLoginDialog
+  })
+
   return (
     <Layout>
       <Profile />
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+      <IdentityModal
+        showDialog={loginModalVisibility}
+        onCloseDialog={() => setLoginModalVisibility(false)}
+      />
     </Layout>
   )
 };
 
-export default Dashbaord;
+export default Dashboard;
